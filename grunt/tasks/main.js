ac9fd974a1a057d9ccded12a9f3e9f3951e56416
Shane Daniel <simrpgman@gmail.com>
2014-11-17 22:33:03 -0600

    inline css into markup

--- grunt/tasks/main.js
+++ grunt/tasks/main.js
@@ -2,6 +2,13 @@
 
 module.exports = function(grunt) {
     grunt.registerTask('main', 'Perform main build tasks.', function(){
-      grunt.task.run(['concurrent:main', 'clean:build']);
+      grunt.task.run([
+        //'concurrent:main',
+        // markup inlines styles now
+        'scripts',
+        'styles',
+        'markup',
+        'clean:build'
+      ]);
     });
 };
